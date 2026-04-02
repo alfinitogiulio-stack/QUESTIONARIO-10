157eef5ac68653ef28bb82e05dc18fcbea620534
alfinitogiulio-stack <alfinitogiulio@gmail.com>
2026-04-02 16:46:52 +0200

    Delete domande_tutto_completo directory

--- domande_tutto_completo/link_stabile.js
+++
@@ -1,4 +0,0 @@
-window.STABLE_LINKS = {
-  domande: "https://INSERISCI-QUI-IL-LINK-ONRENDER.onrender.com/domande.html",
-  statistiche: "https://INSERISCI-QUI-IL-LINK-ONRENDER.onrender.com/statistiche.html",
-};
